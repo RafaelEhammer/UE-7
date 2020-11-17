5c6a10330d402a886fd440c4c5ef29e2bb1e10dc
Rafael Ehammer <Rafael.ehammer@htl-wels.at>
2020-11-17 11:56:59 +0100

    added sample

---
+++ src/sample/CurrencyConverter.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class CurrencyConverter
+{
+    public double euro;
+    public double Yen;
+
+    public double euroToYen (double euro)
+    {
+        Yen = euro * 124;
+        return Yen;
+    }
+}
