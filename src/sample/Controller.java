cfa185d872028ff28052fafe9345a51892acc6db
Rafael Ehammer <Rafael.ehammer@htl-wels.at>
2020-11-17 12:35:05 +0100

    added Button function

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,29 @@
 package sample;
 
-public class Controller
-{
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
+public class Controller extends CurrencyConverter
+{
+    @FXML Button convert = new Button();
+
+    @FXML TextField TextFieldEuro = new TextField();
+    @FXML TextField TextFieldYen = new TextField();
+
+    public void doConvertion()
+    {
+        EventHandler<MouseEvent> handler = new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent mouseEvent)
+            {
+                double x = Double.parseDouble(TextFieldEuro.getText());
+                TextFieldYen.setText(String.valueOf(EuroToYen(x)));
+            }
+        };
+          convert.addEventHandler(MouseEvent.MOUSE_CLICKED, handler);
+    }
 }
--- src/sample/CurrencyConverter.java
+++ src/sample/CurrencyConverter.java
@@ -4,7 +4,7 @@ public class CurrencyConverter
 {
     public double Yen;
 
-    public double euroToYen (double euro)
+    public double EuroToYen (double euro)
     {
         Yen = euro * 124;
         return Yen;
